ca747f3b1be108825fbac45aeccd1484d3b52533
Gustavo Wagner <gugawag@gmail.com>
2022-10-11 15:35:55 -0300

    inicio

---
+++ servidor.py
@@ -0,0 +1,23 @@
+import socket
+from time import sleep
+import threading
+
+HOST = 'localhost'
+PORT = 5000
+
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+udp.bind((HOST, PORT))
+
+print('Servidor no ar...')
+
+def trata_cliente(msg, cliente):
+    print('Recebi de', cliente, 'a mensagem', msg.decode())
+    udp.sendto(('OK-' + msg.decode()).encode(), cliente)
+
+while True:
+    msg, cliente = udp.recvfrom(1024)
+    t = threading.Thread(target=trata_cliente, args=(msg, cliente,))
+    t.start()
+
+udp.close()
